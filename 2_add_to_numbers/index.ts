280817ca82810dd65a69c35de2a2e62315e18277
holyshared <holy.shared.design@gmail.com>
2020-10-29 10:15:59 +0900

    fix test code

--- 2_add_to_numbers/index.ts
+++ 2_add_to_numbers/index.ts
@@ -8,99 +8,97 @@ class ListNode {
 }
 
 
-function toNumber(l: ListNode | null): number {
-  if (l === null) {
-    return 0;
-  }
-  let r = 0;
-  let p = 0;
-  let c = l;
-  while (c.next) {
-    r +=  p === 0 ? c.val : (10 ** p * c.val);
-    c = c.next; 
-    p++;
-  }
-  r +=  p === 0 ? c.val : (10 ** p * c.val);
-
-  return r;
-}
-
-function reverseNumber(num: number): number {
-  let r = 0;
-  let dr = num;
-  while (dr !== 0) {
-    const d = dr % 10;
-    dr = Math.trunc(dr / 10);
-    r = (r * 10) + d;
-  }
-  return r;
-}
-
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  const sum = toNumber(l1) + toNumber(l2);
+  let sum = 0;
+  let current = new ListNode(0);
+  let result = current;
 
-  if (sum <= 10) {
-    return new ListNode(sum);
+  while (l1 || l2) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    sum = sum > 9 ? 1 : 0;
   }
 
-  const rnum = reverseNumber(sum);
-
-  const v = rnum.toString().split('');
-  const r = new ListNode(Number(v[0]));
-  let c = r;
-
-  for (let i = 1; i < v.length; i++) {
-    const n = new ListNode(Number(v[i]));
-    c.next = n;
-    c = n;
+  if (sum) {
+    current.next = new ListNode(sum);
   }
-  return r;
-}
 
-function sum(l: ListNode | null) {
-  if (l === null) {
-    return 0;
-  }
-  let c = l;
-  let v = [];
-  while (c.next) {
-    v.push(c.val);
-    c = c.next;
-  }
-  v.push(c.val);
-  return Number(v.join(''));
-}
+  return result.next;
+};
+
 
 import { assert } from "https://deno.land/std@0.73.0/testing/asserts.ts";
+
+function eq(l1: ListNode | null, l2: ListNode | null) {
+  let c1 = l1;
+  let c2 = l2;
+
+  while (c1 || c2) {
+    if (c1 === null || c2 === null) {
+      return false;
+    }
+    if (c1.val !== c2.val) {
+      return false;
+    }
+    c1 = c1.next;
+    c2 = c2.next;
+  }
+
+  return true;
+}
+
 
 Deno.test("input: l1 = [2,4,3], l2 = [5,6,4]", () => {
   const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
   const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
-  assert(sum(addTwoNumbers(l1, l2)) === 708);
+  const expect = new ListNode(7, new ListNode(0, new ListNode(8)));
+  assert(eq(addTwoNumbers(l1, l2), expect));
 });
 
 
 Deno.test("input: l1 = [101], l2 = [101]", () => {
   const l1 = new ListNode(1, new ListNode(0, new ListNode(1)));
   const l2 = new ListNode(1, new ListNode(0, new ListNode(1)));
-  assert(sum(addTwoNumbers(l1, l2)) === 202);
+  const expect = new ListNode(2, new ListNode(0, new ListNode(2)));
+  assert(eq(addTwoNumbers(l1, l2), expect));
 });
 
 
 Deno.test("input: l1 = [0], l2 = [0]", () => {
   const l1 = new ListNode(0);
   const l2 = new ListNode(0);
-  assert(sum(addTwoNumbers(l1, l2)) === 0);
+  const expect = new ListNode(0);
+  assert(eq(addTwoNumbers(l1, l2), expect));
 });
 
 Deno.test("input: l1 = null, l2 = [0]", () => {
   const l1 = null;
   const l2 = new ListNode(0);
-  assert(sum(addTwoNumbers(l1, l2)) === 0);
+  const expect = new ListNode(0);
+  assert(eq(addTwoNumbers(l1, l2), expect));
 });
 
 Deno.test("input: l1 = [0], l2 = null", () => {
   const l1 = new ListNode(0);
   const l2 = null;
-  assert(sum(addTwoNumbers(l1, l2)) === 0);
+  const expect = new ListNode(0);
+  assert(eq(addTwoNumbers(l1, l2), expect));
+});
+
+Deno.test("input: l1 = [9, 9], l2 = [1]", () => {
+  const l1 = new ListNode(9, new ListNode(9));
+  const l2 = new ListNode(1);
+  const expect = new ListNode(0, new ListNode(0, new ListNode(1)));
+  assert(eq(addTwoNumbers(l1, l2), expect));
 });
