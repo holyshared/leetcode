48a5ae51674894f1441b0184addb054a87412648
holyshared <holy.shared.design@gmail.com>
2020-11-05 20:01:24 +0900

    fix code

--- 28_implement_strstr/index.ts
+++ 28_implement_strstr/index.ts
@@ -3,26 +3,49 @@ function strStr(haystack: string, needle: string): number {
     return 0;
   }
 
-  let i = 0;
+  if (needle === "") {
+    return 0;
+  }
+
   let j = 0; 
+  let i = 0;
   let r = -1;
-  while (i < haystack.length) {
-    if (haystack.substr(i, 1) === needle.substr(j, 1)) {
-      if (j >= needle.length - 1) {
+
+  const p = needle.substr(0, 1);
+
+  const find = (s: string, p: number) => {
+    while (p < haystack.length) {
+      if (haystack.substr(p, 1) === s) {
         break;
       }
-      if (r === -1) {
-        r = i;
+      p++;
+    }
+    return p;
+  };
+
+  while (i < haystack.length) {
+    i = find(p, i);
+    if (i >= haystack.length) {
+      return -1;
+    }
+    r = i;
+
+    while (j < needle.length) {
+      if (haystack.substr(i, 1) === needle.substr(j, 1)) {
+        j++;
+        i++;
+        if (j >= needle.length) {
+          return r;
+        }
+      } else {
+        i = r + 1;
+        j = 0;
+        break;
       }
-      i++;
-      j++;
-    } else {
-      i++;
-      j = 0;
-      r = -1;
     }
   }
-  return r;
+
+  return -1;
 }
 
 import { assert } from "https://deno.land/std@0.73.0/testing/asserts.ts";
@@ -43,6 +66,18 @@ Deno.test("haystack = abcde, needle = ab", () => {
   assert(strStr("abcde", "ab") === 0);
 });
 
+Deno.test("haystack = ab, needle = a", () => {
+  assert(strStr("ab", "a") === 0);
+});
+
 Deno.test("haystack = '', needle = ''", () => {
   assert(strStr("", "") === 0);
+});
+
+Deno.test("haystack = 'a', needle = ''", () => {
+  assert(strStr("a", "") === 0);
+});
+
+Deno.test("haystack = 'mississippi', needle = 'issip'", () => {
+  assert(strStr("mississippi", "issip") === 4);
 });
