94e5523ef4ca7e17256dfcfdf54b4e3c59385cf8
holyshared <holy.shared.design@gmail.com>
2020-12-06 22:58:46 +0900

    binary search

--- 35_search_insert_position/index.ts
+++ 35_search_insert_position/index.ts
@@ -1,39 +1,37 @@
 function searchInsert(nums: number[], target: number): number {
-  let i = 0;
+  let pivot, left = 0, right = nums.length - 1;
 
-  for (; i < nums.length; i++) {
-    if (nums[i] === target) {
-      return i;
-    } else if (nums[i] > target) {
-      return i;
+  while (left <= right) {
+    pivot = left + Math.round((right - left) / 2);
+    if (nums[pivot] === target) {
+      return pivot;
+    } else if (target < nums[pivot]) {
+      right = pivot - 1;
     } else {
-      continue;
+      left = pivot + 1;
     }
   }
-
-  return i;
+  return left;
 }
 
-import { assert } from "https://deno.land/std@0.73.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.73.0/testing/asserts.ts";
 
 Deno.test("nums = [1,3,5,6], target = 5", () => {
-  assert(searchInsert([1,3,5,6], 5) === 2);
+  assertEquals(searchInsert([1,3,5,6], 5), 2);
 });
 
 Deno.test("nums = [1,3,5,6], target = 2", () => {
-  const pos = searchInsert([1,3,5,6], 2);
-  console.log(pos);
-  assert(pos === 1);
+  assertEquals(searchInsert([1,3,5,6], 2), 1);
 });
 
 Deno.test("nums = [1,3,5,6], target = 7", () => {
-  assert(searchInsert([1,3,5,6], 7) === 4);
+  assertEquals(searchInsert([1,3,5,6], 7), 4);
 });
 
 Deno.test("nums = [1,3,5,6], target = 0", () => {
-  assert(searchInsert([1,3,5,6], 0) === 0);
+  assertEquals(searchInsert([1,3,5,6], 0), 0);
 });
 
 Deno.test("nums = [1], target = 0", () => {
-  assert(searchInsert([1], 0) === 0);
+  assertEquals(searchInsert([1], 0), 0);
 });
