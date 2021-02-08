a732c9620ca9c9db71e6725daf30999fafd59313
holyshared <holy.shared.design@gmail.com>
2021-02-08 14:45:11 +0900

    fix code

--- 88_merge_sorted_array/index.ts
+++ 88_merge_sorted_array/index.ts
@@ -1,8 +1,29 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  for (let i = 0; i < n; i++) {
-    nums1[i + m] = nums2[i];
+  const nums1Clone = [...nums1]; 
+
+  let p1 = 0; // num1 pointer
+  let p2 = 0; // num2 pointer
+
+  for (let p = 0; p < m + n; p++) {
+    // 1. マージしなければならない項目がない
+    // 2. 現在の項目より、追加しなければならない項目の方が大きい
+    //    AND
+    //    既存の項目がまだある
+    // nums1 = [1,2,3,0,0,0]
+    // nums2 = [2,5,6]
+    //
+    // [ 1, 2, 3, 0, 0, 0 ]
+    // [ 1, 2, 2, 0, 0, 0 ]
+    // [ 1, 2, 2, 3, 0, 0 ]
+    // [ 1, 2, 2, 3, 5, 0 ]
+    // [ 1, 2, 2, 3, 5, 6 ]
+    //
+    if (p2 >= n || (p1 < m && nums1Clone[p1] < nums2[p2])) {
+      nums1[p] = nums1Clone[p1++];
+    } else {
+      nums1[p] = nums2[p2++];
+    }
   }
-  nums1.sort((a, b) => a - b);
 }
 
 
