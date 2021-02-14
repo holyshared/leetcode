3dd4a37740e49cf64e0f6b0d40fc4ed6f7037347
holyshared <holy.shared.design@gmail.com>
2021-02-14 17:01:48 +0900

    fix code

--- 108_convert_sorted_array_to_binary_search_tree/main.go
+++ 108_convert_sorted_array_to_binary_search_tree/main.go
@@ -10,42 +10,41 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 0 + (5 - 0) -10, -3, 0, 5, 9
+// 0 + (2 - 0) -10, -3
+func _sortedArrayToBST(nums []int, left int, right int) *TreeNode {
+	if (left > right) {
+		return nil
+	}
+	mid := left + (right - left) / 2
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = _sortedArrayToBST(nums, left, mid - 1)
+	root.Right = _sortedArrayToBST(nums, mid + 1, right)
+	return root
+}
+
 func sortedArrayToBST(nums []int) *TreeNode {
-	var root *TreeNode
 	if len(nums) <= 0 {
 		return nil
 	}
-
-	if len(nums) == 1 {
-		root = &TreeNode{Val: nums[0]}
-		return root
-	}
-
-	if len(nums)%2 == 0 && len(nums) == 2 {
-		root = &TreeNode{Val: nums[1], Left: &TreeNode{Val: nums[0]}}
-		return root
-	}
-	mid := len(nums) / 2
-	root = &TreeNode{Val: nums[mid]}
-	left := sortedArrayToBST(nums[:mid])
-	right := sortedArrayToBST(nums[(mid + 1):])
-	root.Left = left
-	root.Right = right
-	return root
+	return _sortedArrayToBST(nums, 0, len(nums) - 1);
 }
 
 func main() {
 	/*
-		tree := sortedArrayToBST([]int{-10, -3, 0, 5, 9})
-		fmt.Println(tree.Val)
-		fmt.Println(tree.Left.Val)
-		fmt.Println(tree.Left.Left.Val)
-		fmt.Println(tree.Right.Val)
-		fmt.Println(tree.Right.Left.Val)
-	*/
-
-	tree := sortedArrayToBST([]int{-1, 0, 1, 2})
+	tree := sortedArrayToBST([]int{-10, -3, 0, 5, 9})
 	fmt.Println(tree.Val)
+	fmt.Println(tree.Left.Val)
+	fmt.Println(tree.Left.Left.Val)
+	fmt.Println(tree.Right.Val)
+	fmt.Println(tree.Right.Left.Val)
+*/
+	tree1 := sortedArrayToBST([]int{-1, 0, 1, 2})
+	fmt.Println(tree1.Val)
+	fmt.Println(tree1.Left.Val)
+	fmt.Println(tree1.Right.Val)
+	fmt.Println(tree1.Right.Right.Val)
 
-	//	[-1,0,1,2]
+	tree2 := sortedArrayToBST([]int{})
+	fmt.Println(tree2)
 }
