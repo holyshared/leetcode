5a86b4670b689eaf074f7b89f620aab5675031e9
holyshared <holy.shared.design@gmail.com>
2021-03-04 19:12:07 +0900

    add new solution

--- intersection_of_two_linked_lists/main.go
+++ intersection_of_two_linked_lists/main.go
@@ -6,32 +6,21 @@ type ListNode struct {
 }
 
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-	if headA == nil || headB == nil {
-		return nil
-	}
-
-	steps := map[*ListNode]bool{}
 	acurr, bcurr := headA, headB
-	ok := false
 
-	for acurr != nil || bcurr != nil {
-		if acurr != nil {
-			_, ok = steps[acurr]
-			if ok {
-				return acurr
-			}
-			steps[acurr] = true
+	for acurr != bcurr {
+		if acurr == nil {
+			acurr = headB
+		} else {
 			acurr = acurr.Next
 		}
-		if bcurr != nil {
-			_, ok = steps[bcurr]
-			if ok {
-				return bcurr
-			}
-			steps[bcurr] = true
+
+		if bcurr == nil {
+			bcurr = headA
+		} else {
 			bcurr = bcurr.Next
 		}
 	}
 
-	return nil
+	return acurr
 }
