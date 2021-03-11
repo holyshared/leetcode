833f85b1cc4d88e6450d24560e1f288925e09999
holyshared <holy.shared.design@gmail.com>
2021-03-11 19:37:32 +0900

    add comment

--- coin_change/main.go
+++ coin_change/main.go
@@ -1,33 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
-func _coinChange(coins []int, remain int, memo map[int]int) int {
+// memo[x]は残額をキーとしたコインの最小枚数
+// キーが残額で、値がコインの枚数
+func _coinChange(coins []int, remain int, memo []int) int {
 	if remain < 0 {
 		return -1
 	}
 	if remain == 0 {
 		return 0
 	}
-	num, ok := memo[remain-1]
-	if ok {
-		return num
+	// すでに残額からの最小枚数がある場合それを返す
+	// 最小枚数かどうかは補償されていない
+	// 他の消費パターンで更新される可能性があるため
+	if memo[remain-1] != 0 {
+		return memo[remain-1]
 	}
 	min := math.MaxInt64
 	for _, c := range coins {
 		result := _coinChange(coins, remain-c, memo)
+		// コインを消費できた && 現在の最小値より少ない枚数?
 		if result >= 0 && result < min {
-			min = 1 + result
+			min = 1 + result // コインの枚数を増やす
 		}
 	}
 	mr := 0
+	// 手持ちのコインだと0にできない
 	if min == math.MaxInt64 {
 		mr = -1
 	} else {
 		mr = min
 	}
+	// 今の残額での最小枚数
 	memo[remain-1] = mr
 
 	return mr
@@ -37,13 +45,10 @@ func coinChange(coins []int, amount int) int {
 	if amount <= 0 {
 		return 0
 	}
-	memo := map[int]int{}
-	_coinChange(coins, amount, memo)
+	bucket := make([]int, amount)
+	result := _coinChange(coins, amount, bucket)
 
-	v, ok := memo[amount-1]
-	if ok {
-		return v
-	} else {
-		return -1
-	}
+	fmt.Println(bucket)
+
+	return result
 }
