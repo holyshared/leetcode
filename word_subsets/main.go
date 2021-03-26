f6b0a7a21e6be574d13bf585ca6d5774746eeadf
holyshared <holy.shared.design@gmail.com>
2021-03-26 22:37:25 +0900

    add fix code

--- word_subsets/main.go
+++ word_subsets/main.go
@@ -1,56 +1,40 @@
 package main
 
-type CharCount map[rune]int
+import (
+	"math"
+)
 
-func calcCharCount(words []string) map[string]CharCount {
-	charCount := map[string]CharCount{}
-	for i := 0; i < len(words); i++ {
-		chars := []rune(words[i])
-		count := CharCount{}
-		for _, c := range chars {
-			curr, ok := count[c]
-			if ok {
-				count[c] = curr + 1
-			} else {
-				count[c] = 1
-			}
-		}
-		charCount[words[i]] = count
+func calcCharCount(word string) []int {
+	ans := make([]int, 26)
+	for _, c := range []rune(word) {
+		ans[int(c-'a')]++
 	}
-	return charCount
-}
-
-func isInclude(ac CharCount, bc CharCount) bool {
-	for c, bcount := range bc {
-		acount, ok := ac[c]
-		// aにbがない
-		if !ok {
-			return false
-		}
-		if acount < bcount {
-			return false
-		}
-	}
-	return true
+	return ans
 }
 
 func wordSubsets(A []string, B []string) []string {
-	a := calcCharCount(A)
-	b := calcCharCount(B)
+	bmax := calcCharCount("")
 
-	results := []string{}
+	for _, b := range B {
+		bCount := calcCharCount(b)
+		for i := 0; i < 26; i++ {
+			bmax[i] = int(math.Max(float64(bmax[i]), float64(bCount[i])))
+		}
+	}
 
-	for aw, awc := range a {
+	ans := []string{}
+	for _, a := range A {
 		matchAll := true
-		for _, bwc := range b {
-			if !isInclude(awc, bwc) {
+		aCount := calcCharCount(a)
+		for i := 0; i < 26; i++ {
+			if aCount[i] < bmax[i] {
 				matchAll = false
 				break
 			}
 		}
 		if matchAll {
-			results = append(results, aw)
+			ans = append(ans, a)
 		}
 	}
-	return results
+	return ans
 }
