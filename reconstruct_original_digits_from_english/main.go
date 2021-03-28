1f7e29a459143a28939404bded26cfd065a9c61b
holyshared <holy.shared.design@gmail.com>
2021-03-28 21:32:15 +0900

    add comment

--- reconstruct_original_digits_from_english/main.go
+++ reconstruct_original_digits_from_english/main.go
@@ -14,16 +14,16 @@ func originalDigits(s string) string {
 	source := countChars(s)
 
 	out := make([]int, 10)
-	out[0] = source[int('z'-'a')]
-	out[2] = source[int('w'-'a')]
-	out[4] = source[int('u'-'a')]
-	out[6] = source[int('x'-'a')]
-	out[8] = source[int('g'-'a')]
+	out[0] = source[int('z'-'a')] // zero
+	out[2] = source[int('w'-'a')] // two
+	out[4] = source[int('u'-'a')] // four
+	out[6] = source[int('x'-'a')] // six
+	out[8] = source[int('g'-'a')] // eight
 
-	out[3] = source[int('h'-'a')] - out[8]
-	out[5] = source[int('f'-'a')] - out[4]
-	out[7] = source[int('s'-'a')] - out[6]
-	out[9] = source[int('i'-'a')] - out[5] - out[6] - out[8]
+	out[3] = source[int('h'-'a')] - out[8]                   // three
+	out[5] = source[int('f'-'a')] - out[4]                   // five
+	out[7] = source[int('s'-'a')] - out[6]                   // seven
+	out[9] = source[int('i'-'a')] - out[5] - out[6] - out[8] // nine
 	out[1] = source[int('n'-'a')] - out[7] - 2*out[9]
 
 	result := ""
