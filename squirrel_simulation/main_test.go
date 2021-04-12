ac98e94907b7742a8f1b176fce905c29b901f8fc
holyshared <holy.shared.design@gmail.com>
2021-04-12 18:52:49 +0900

    add squirrel_simulation

---
+++ squirrel_simulation/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestA(t *testing.T) {
+	distance := minDistance(
+		5,
+		7,
+		[]int{2, 2},
+		[]int{4, 4},
+		[][]int{{3, 0}, {2, 5}},
+	)
+	if distance != 12 {
+		t.Fatalf("%d", distance)
+	}
+}
