3f540d12511f157db0873ec74a2ae680dd071130
holyshared <holy.shared.design@gmail.com>
2021-04-26 19:42:13 +0900

    fix furthest_building_you_can_reach

--- furthest_building_you_can_reach/main.go
+++ furthest_building_you_can_reach/main.go
@@ -1,34 +1,42 @@
 package main
 
-func _furthestBuilding(heights []int, curr int, remainBricks int, remainLadders int) int {
-	if curr >= len(heights)-1 {
-		return curr
-	}
+import "sort"
 
-	if heights[curr] >= heights[curr+1] {
-		return _furthestBuilding(heights, curr+1, remainBricks, remainLadders)
-	}
+func isReachable(buildingIndex int, heights []int, bricks int, ladders int) bool {
+	climbs := []int{}
 
-	need := heights[curr+1] - heights[curr]
-	// レンガは使える
-	canUseBricks := need < remainBricks
-	b := curr
-	if canUseBricks {
-		b = _furthestBuilding(heights, curr+1, remainBricks-need, remainLadders)
+	for i := 0; i < buildingIndex; i++ {
+		h1 := heights[i]
+		h2 := heights[i+1]
+		if h2 <= h1 {
+			continue
+		}
+		climbs = append(climbs, h2-h1)
 	}
-	canUseRemainLadders := remainLadders > 0
-	l := curr
-	if canUseRemainLadders {
-		l = _furthestBuilding(heights, curr+1, remainBricks, remainLadders-1)
-	}
+	sort.Sort(sort.IntSlice(climbs))
 
-	if l < b {
-		return b
-	} else {
-		return l
+	for _, climb := range climbs {
+		if climb <= bricks {
+			bricks -= climb
+		} else if ladders >= 1 {
+			ladders -= 1
+		} else {
+			return false
+		}
 	}
+	return true
 }
 
 func furthestBuilding(heights []int, bricks int, ladders int) int {
-	return _furthestBuilding(heights, 0, bricks, ladders)
+	lo := 0
+	hi := len(heights) - 1
+	for lo < hi {
+		mid := lo + (hi-lo+1)/2
+		if isReachable(mid, heights, bricks, ladders) {
+			lo = mid
+		} else {
+			hi = mid - 1
+		}
+	}
+	return hi
 }
