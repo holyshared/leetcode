9d8457892cb66bdc94f58e99d2078a6125ad7dd9
holyshared <holy.shared.design@gmail.com>
2021-05-04 16:35:11 +0900

    add search_in_rotated_sorted_array

---
+++ search_in_rotated_sorted_array/main.go
@@ -0,0 +1,26 @@
+package main
+
+func search(nums []int, target int) int {
+	start := 0
+	end := len(nums) - 1
+
+	for start <= end {
+		mid := start + (end-start)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] >= nums[start] {
+			if target >= nums[start] && target < nums[mid] {
+				end = mid - 1
+			} else {
+				start = mid + 1
+			}
+		} else {
+			if target <= nums[end] && target > nums[mid] {
+				start = mid + 1
+			} else {
+				end = mid - 1
+			}
+		}
+	}
+	return -1
+}
