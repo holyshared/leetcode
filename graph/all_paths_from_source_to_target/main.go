31e3a408abfc1d586df08bb49977fbb2680b386a
holyshared <holy.shared.design@gmail.com>
2022-07-11 18:45:43 +0900

    fix code

--- graph/all_paths_from_source_to_target/main.go
+++ graph/all_paths_from_source_to_target/main.go
@@ -2,17 +2,13 @@ package main
 
 func dfs(graph [][]int, curr int, routes []int, allRoutes [][]int) [][]int {
 	if len(graph) - 1 == curr {
-		routes = append(routes, curr)
 		return append(allRoutes, routes)
-	} else if len(graph[curr]) == 0 {
-		return allRoutes
 	}
-
-	routes = append(routes, curr)
 
 	for _, next := range graph[curr] {
 		clonedRoutes := make([]int, len(routes))
 		copy(clonedRoutes, routes)
+		clonedRoutes = append(clonedRoutes, next)
 		allRoutes = dfs(graph, next, clonedRoutes, allRoutes)
 	}
 
@@ -20,5 +16,5 @@ func dfs(graph [][]int, curr int, routes []int, allRoutes [][]int) [][]int {
 }
 
 func allPathsSourceTarget(graph [][]int) [][]int {
-	return dfs(graph, 0, []int{}, [][]int{})
+	return dfs(graph, 0, []int{0}, [][]int{})
 }
