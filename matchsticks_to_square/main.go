cdb36626a4ea323e1c29b2c4c0355c3d93e3aaa8
holyshared <holy.shared.design@gmail.com>
2022-07-12 21:19:02 +0900

    fix

--- matchsticks_to_square/main.go
+++ matchsticks_to_square/main.go
@@ -2,25 +2,6 @@ package main
 
 import "sort"
 
-func _makesquare(matchsticks []int, curr int, squareSideLength int, square []int) bool {
-	if curr > len(matchsticks)-1 {
-		return square[0] == square[1] && square[1] == square[2] && square[2] == square[3]
-	}
-
-	for i := 0; i < 4; i++ {
-		if square[i]+matchsticks[i] > squareSideLength {
-			continue
-		}
-		square[i] += matchsticks[curr]
-
-		if _makesquare(matchsticks, curr+1, squareSideLength, square) {
-			return true
-		}
-		square[i] -= matchsticks[curr]
-	}
-	return false
-}
-
 func makesquare(matchsticks []int) bool {
 	sum := 0
 	for i := 0; i < len(matchsticks); i++ {
@@ -34,8 +15,26 @@ func makesquare(matchsticks []int) bool {
 	square := make([]int, 4)
 
 	sort.Slice(matchsticks, func (i, j int) bool {
-		return matchsticks[i] < matchsticks[j]
+		return matchsticks[i] > matchsticks[j]
 	})
-
-	return _makesquare(matchsticks, 0, squareSideLength, square)
+	var _makesquare func (curr int) bool
+	_makesquare = func (curr int) bool {
+		if curr > len(matchsticks)-1 {
+			return square[0] == square[1] && square[1] == square[2] && square[2] == square[3]
+		}
+	
+		for i := 0; i < 4; i++ {
+			if square[i]+matchsticks[curr] > squareSideLength {
+				continue
+			}
+			square[i] += matchsticks[curr]
+	
+			if _makesquare(curr+1) {
+				return true
+			}
+			square[i] -= matchsticks[curr]
+		}
+		return false
+	}
+	return _makesquare(0)
 }
--- matchsticks_to_square/main_test.go
+++ matchsticks_to_square/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-/*
+
 func TestA(t *testing.T) {
 	actual := makesquare([]int{1, 1, 2, 2, 2})
 	if actual != true {
@@ -18,7 +18,7 @@ func TestB(t *testing.T) {
 	}
 
 }
-*/
+
 func TestC(t *testing.T) {
 	actual := makesquare([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, 2, 1})
 	if actual != false {
