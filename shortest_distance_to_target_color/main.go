df8b493f4438c7b5bc5b564b934ba161090bc1d2
holyshared <holy.shared.design@gmail.com>
2022-07-15 14:07:51 +0900

    fix code

--- shortest_distance_to_target_color/main.go
+++ shortest_distance_to_target_color/main.go
@@ -1,38 +1,52 @@
 package main
 
-import "math"
+import (
+	"sort"
+)
 
 func shortestDistanceColor(colors []int, queries [][]int) []int {
-	distances := make([][]int, len(colors))
 
-	for i := 0; i < len(distances); i++ {
-		distances[i] = []int{math.MaxInt, math.MaxInt, math.MaxInt}
-	}
+	colorDistances := make([][]int, 3)
 
 	for i := 0; i < len(colors); i++ {
-		for j := 0; j < len(colors); j++ {
-			abs := 0
-			if i > j {
-				abs = i - j
-			} else {
-				abs = j - i
-			}
-			if abs < distances[i][colors[j]-1] {
-				distances[i][colors[j]-1] = abs
-			}
-		}
+		colorIndex := colors[i] - 1
+		colorDistances[colorIndex] = append(colorDistances[colorIndex], i)
 	}
 
 	results := make([]int, len(queries))
 
 	for i := 0; i < len(queries); i++ {
-		distance := distances[queries[i][0]][queries[i][1]-1]
+		index, color := queries[i][0], queries[i][1]
 
-		if distance == math.MaxInt {
+		if len(colorDistances[color - 1]) <= 0 {
 			results[i] = -1
+			continue
+		}
+
+		indexes := colorDistances[color - 1]
+		insert := sort.SearchInts(indexes, index)
+
+		if insert < 0 {
+			insert = (insert + 1) * -1
+		}
+
+		// 一番先頭になる場合
+		if insert == 0 {
+			results[i] = indexes[insert] - index
+		// 一番末尾になる場合
+		} else if (insert == len(indexes)) {
+			results[i] = index - indexes[insert - 1]
 		} else {
-			results[i] = distance
+			leftNearest := index - indexes[insert - 1]
+			rightNearest := indexes[insert] - index
+
+			if leftNearest > rightNearest {
+				results[i] = rightNearest
+			} else {
+				results[i] = leftNearest
+			}
 		}
 	}
+
 	return results
 }
