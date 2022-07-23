eccc9022824f0eb237aaeff7b8d755043d465c81
holyshared <holy.shared.design@gmail.com>
2022-07-23 13:06:23 +0900

    add reverse_linked_list_2

--- reverse_linked_list_2/main.go
+++ reverse_linked_list_2/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -21,52 +19,40 @@ func reverse(head *ListNode) (*ListNode, *ListNode) {
 	curr.Next = prev
 	head.Next = nil
 
-	fmt.Println("last--")
-	fmt.Println(head)
-
 	return curr, head
 }
 
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	if left == right {
+		return head
+	}
+
 	top := head
 	start := head
 
-	for i := 0; i < left - 1; i++ {
+	for i := 0; i < left-1; i++ {
 		top = start        // 1
 		start = start.Next // 2
 	}
 
-	//curr := start
 	end := start
-	for i := 0; i < right - left; i++ {
+	for i := 0; i < right-left; i++ {
 		end = end.Next
 	}
 	bottom := end.Next
 
-//	fmt.Println(start.Val)
-//	fmt.Println(start.Next.Val)
-//	fmt.Println(start.Next.Next.Val)
-
 	end.Next = nil
-
-	//fmt.Println(start.Next.Val)
 
 	a, b := reverse(start)
 
-	top.Next = a
-	b.Next = bottom
+	if left-1 > 0 {
+		top.Next = a
+		b.Next = bottom
+		return head
+	} else {
+		top = a
+		b.Next = bottom
+		return top
+	}
 
-//	fmt.Println(top)
-	//fmt.Println(bottom)
-
-///	a.Next = bottom
-/*
-	fmt.Println(top.Val)
-	fmt.Println(start.Val)
-	fmt.Println(start.Next.Val)
-
-	fmt.Println(end.Val)
-	fmt.Println(bottom.Val)
-*/
-	return top
 }
--- reverse_linked_list_2/main_test.go
+++ reverse_linked_list_2/main_test.go
@@ -31,12 +31,47 @@ func match(actual, expected []int) bool {
 	return true
 }
 
-
 func TestA(t *testing.T) {
 	root := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}
-	expected := []int{1, 4,3,2, 5}
+	expected := []int{1, 4, 3, 2, 5}
 
 	actual := reverseBetween(root, 2, 4)
+	actualList := nodeToList(actual)
+
+	if !match(actualList, expected) {
+		t.Fatalf("actual = %v, expected = %v", actualList, expected)
+	}
+}
+
+func TestB(t *testing.T) {
+	root := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}
+	expected := []int{2, 1, 3, 4, 5}
+
+	actual := reverseBetween(root, 1, 2)
+	actualList := nodeToList(actual)
+
+	if !match(actualList, expected) {
+		t.Fatalf("actual = %v, expected = %v", actualList, expected)
+	}
+}
+
+func TestC(t *testing.T) {
+	root := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}
+	expected := []int{1, 2, 3, 5, 4}
+
+	actual := reverseBetween(root, 4, 5)
+	actualList := nodeToList(actual)
+
+	if !match(actualList, expected) {
+		t.Fatalf("actual = %v, expected = %v", actualList, expected)
+	}
+}
+
+func TestD(t *testing.T) {
+	root := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}
+	expected := []int{1, 2, 3, 4, 5}
+
+	actual := reverseBetween(root, 1, 1)
 	actualList := nodeToList(actual)
 
 	if !match(actualList, expected) {
